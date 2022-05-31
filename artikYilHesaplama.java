5bf252edbc40fcd37bd3639ff46c62215a0ee7fb
Hatice GAZEL <98157466+gazellhatice@users.noreply.github.com>
2022-05-31 22:05:06 +0300

    Add files via upload

---
+++ artikYilHesaplama.java
@@ -0,0 +1,32 @@
+//https://app.patika.dev/gazellhatice
+
+import java.util.Scanner;
+
+public class artikYilHesaplama {
+
+    public static void main(String[] args) {
+
+        int yil = 2018;
+        boolean artik = false;
+
+        if(yil % 4 == 0)
+        {
+            if( yil % 100 == 0)
+            {
+                if ( yil % 400 == 0)
+                    artik = true;
+                else
+                    artik = false;
+            }
+            else
+                artik = true;
+        }
+        else
+            artik = false;
+
+        if(artik)
+            System.out.println(yil + " artık yıldır.");
+        else
+            System.out.println(yil + " artık yıl değildir.");
+    }
+}
